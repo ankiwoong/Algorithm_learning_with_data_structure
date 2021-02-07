6e588402ec8f0f13fae721578324c918598dffc6
AnKiWoong <ankiwoong@gmail.com>
2021-02-08 08:56:11 +0900

    체인법 - ChainedHash 해시 클래스 만들기

--- Chap03/chained_hash.py
+++ Chap03/chained_hash.py
@@ -12,3 +12,18 @@ class Node:
         self.key = key  # 키
         self.value = value  # 값
         self.next = next  # 뒤쪽 노드를 참조
+
+
+class ChainedHash:
+    """체인법을 해시 클래스 구현"""
+
+    def __init__(self, capacity: int) -> None:
+        """초기화"""
+        self.capacity = capacity  # 해시 테이블의 크기를 지정
+        self.table = [None] * self.capacity  # 해시 테이블(리스트)을 선언
+
+    def hash_value(self, key: Any) -> int:
+        """해시값을 구함"""
+        if isinstance(key, int):
+            return key % self.capacity
+        return int(hashlib.sha256(str(key).encode()).hexdigest(), 16) % self.capacity
